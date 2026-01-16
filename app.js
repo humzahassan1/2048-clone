3e375a236af8e9f09b56803a8ca6aff3840258f9
humzahassan1 <156954770+humzahassan1@users.noreply.github.com>
2026-01-16 00:31:17 -0500

    modified html added some css

---
+++ app.js
@@ -0,0 +1,6 @@
+document.title  = "project_2048";
+document.body.style.backgroundColor = "hsl(204, 59%, 58%)";
+console.log(document);
+const items = document.querySelectorAll(".item");
+items[0].style.backgroundColor = "yellow";
+items[0].innerText = "2";
