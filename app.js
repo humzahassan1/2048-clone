dadbacbc041213ff63afbc9b037dd2163d35f7d4
humzahassan1 <156954770+humzahassan1@users.noreply.github.com>
2026-01-16 01:31:40 -0500

    fixed up html mistakes, completely redid javascript in a different way, added some more styling and changed up colors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-document.title  = "project_2048";
-document.body.style.backgroundColor = "hsl(204, 59%, 58%)";
-console.log(document);
-const items = document.querySelectorAll(".item");
-items[0].style.backgroundColor = "yellow";
-items[0].innerText = "2";
+document.addEventListener('DOMContentLoaded', () => {
+    const gridDisplay = document.querySelector('.grid')
+    const scoreDisplay = document.getElementById('score')
+    const resultDisplay = document.getElementById('result')
+
+    console.log(gridDisplay)
+})
