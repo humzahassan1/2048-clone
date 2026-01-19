c7c9d00bd9ca0b26b7ca41865db6f9a51b129250
humzahassan1 <156954770+humzahassan1@users.noreply.github.com>
2026-01-19 16:47:10 -0500

    implememnted combineing row logic and move left and right functions

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const resultDisplay = document.querySelector('#result')
     width = 4
     let squares = []
+    let score = 0
 //Game boaed
     function createBoard(){
     for(let i = 0; i < width * width; i++) {
@@ -51,6 +52,48 @@ document.addEventListener('DOMContentLoaded', () => {
             
         } 
     }
+
+    function moveLeft(){
+        for(let i = 0; i < 16; i++){
+            if(i % 4 === 0){
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i+1].innerHTML
+                let totalThree = squares[i+2].innerHTML
+                let totalFour = squares[i+3].innerHTML
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+                let filteredRow = row.filter(num => num)
+                let missing = 4 - filteredRow.length
+                let zeros = Array(missing).fill(0)
+                let newRow = filteredRow.concat(zeros)
+                console.log(newRow)
+                squares[i].innerHTML = newRow[0]
+                squares[i+1].innerHTML = newRow[1]
+                squares[i+2].innerHTML = newRow[2]
+                squares[i+3].innerHTML = newRow[3]
+
+            }
+            
+        } 
+    }
+
+
+
+
+
+    function combineRow() {
+        for(let i = 0; i < 15; i++){
+            if(squares[i].innerHTML === squares[i+1].innerHTML){
+                let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i+1].innerHTML)
+                squares[i].innerHTML = combinedTotal
+                squares[i+1].innerHTML = 0
+                score += combinedTotal
+                scoreDisplay.innerHTML = score 
+            }
+        }
+        // checkForWin()
+    }
+
         //assigning keys
 
         function control(e){
@@ -60,7 +103,21 @@ document.addEventListener('DOMContentLoaded', () => {
                 keyRight()
             }
         }   
+        document.addEventListener('keydown', control)
+        
+        function keyLeft(){
+        moveLeft()
+        combineRow()
+        moveLeft()
+        generate()
+        }
 
-
+        function keyRight(){
+            moveRight()
+            combineRow()
+            moveRight()
+            generate()
+            }
+    
 
 })
